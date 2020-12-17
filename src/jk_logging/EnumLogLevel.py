b83b417eabaff7a59ba091532a9fbf4ade2b44f3
jkpubsrc <pubsrc@binary-overflow.de>
2020-12-17 16:48:20 +0100

    Reformatted

--- src/jk_logging/EnumLogLevel.py
+++ src/jk_logging/EnumLogLevel.py
@@ -1,5 +1,4 @@
-﻿#!/usr/bin/env python3
-
+﻿
 
 
 import os
@@ -9,7 +8,11 @@ from enum import Enum
 
 
 
+
+
+
 class EnumLogLevel(Enum):
+
 	TRACE = 10, 'TRACE'
 	DEBUG = 20, 'DEBUG'
 	NOTICE = 30, 'NOTICE'
@@ -21,20 +24,49 @@ class EnumLogLevel(Enum):
 	STDERR = 71, 'STDERR'
 	EXCEPTION = 80, 'EXCEPTION'
 
+	################################################################################################################################
+	## Constructor
+	################################################################################################################################
+
 	def __new__(cls, value, name):
 		member = object.__new__(cls)
 		member._value_ = value
 		member.fullname = name
 		return member
+	#
 
+	################################################################################################################################
+	## Public Properties
+	################################################################################################################################
+
+	################################################################################################################################
+	## Public Methods
+	################################################################################################################################
+
+	#
+	# Return an integer representation of this enumeration value.
+	#
 	def __int__(self):
 		return self._value_
+	#
 
+	#
+	# Return a string representation of this enumeration value.
+	#
 	def __str__(self):
 		return self.fullname
+	#
 
+	#
+	# Return a JSON compatible value for this enumeration value.
+	#
 	def toJSON(self):
 		return self._value_
+	#
+
+	################################################################################################################################
+	## Public Static Methods
+	################################################################################################################################
 
 	#
 	# This method converts a string or integer representing a log level back to an enumeration instance.
@@ -47,6 +79,12 @@ class EnumLogLevel(Enum):
 			if data in EnumLogLevel.__dict__["_member_names_"]:
 				return EnumLogLevel.__dict__[data]
 		raise Exception("Unrecognized enumeration value: " + str(data))
+	#
+
+#
+
+
+
 
 
 
